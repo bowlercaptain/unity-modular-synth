a9f02c6d0fa8b71cde1b5a670f360a2f4b744b72
Robert Mock <robertjamesmock@gmail.com>
2017-06-03 19:08:36 -0700

    created curve oscillator

---
+++ Assets/curveOscillator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class curveOscillator : Oscillator {
+
+    [Tooltip("Output values between -1 and 1, one full cycle should be from 0 to 1 horizontally. Use pingpong or loop endpoints!")]
+    public AnimationCurve curve;
+
+    public override float waveFunction(float position)
+    {
+        return curve.Evaluate(position);
+    }
+}
