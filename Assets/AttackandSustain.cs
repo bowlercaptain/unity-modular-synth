41989f32d382e5eff7eb075718df28bd9ffb5ead
Robert Mock <robertjamesmock@gmail.com>
2017-06-10 18:10:56 -0700

    fixed shit, tested with actual keyboard
    shit's jank with volume and stuff, need a better way than the rack to
    make it happen

---
+++ Assets/AttackandSustain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackandSustain : MonoBehaviour, mono {
+
+	public Component _input;
+	private mono input {
+	get { return (mono)_input; }
+	set { _input = (Component)value; }
+	}
+
+	public float attackTime;
+	public float sustainTime;
+
+	private float current;
+	public float[] getSignal(int length)
+	{
+		float[] fill = new float[length];
+		float[] datt = input.getSignal(length);
+		for(int i = 0; i < datt.Length; i++) {
+			if (datt[i] > current)
+			{
+				float attackStep = datt[i] / attackTime / info.sampleRate;
+				current = Mathf.Min(current + attackStep, datt[i]);
+			}
+			if (datt[i] < current)
+			{
+				float sustainStep = 1 / sustainTime / info.sampleRate;//SHIT
+				current = Mathf.Max(current - sustainStep, datt[i]);
+			}
+			fill[i] = current;
+		}
+
+			return fill;
+	}
+}
--- Assets/OscillatorRack.cs
+++ Assets/OscillatorRack.cs
@@ -7,8 +7,8 @@ public class OscillatorRack : MonoBehaviour
 
 	public MidiChannel channel;
 
-	private Stack<GameObject> unusedOscillators;
-	private Dictionary<int, GameObject> playingOscillators;
+	private Stack<GameObject> unusedOscillators = new Stack<GameObject>();
+	private Dictionary<int, GameObject> playingOscillators = new Dictionary<int, GameObject>();
 	public GameObject oscPrefab;//has an InBridge and an out bridge. Clone these and keep track of them, direct them to an addandlevel er
 
 	public AddAndLevel mixer;
@@ -34,15 +34,26 @@ public class OscillatorRack : MonoBehaviour
 	{
 		if (unusedOscillators.Count > 0)
 		{
+			Debug.Log("osc exists");
 			return unusedOscillators.Pop();
 		} else
 		{
-			return Instantiate(oscPrefab);
+			Debug.Log("new osc");
+			var osc = Instantiate(oscPrefab);
+			var temp = new List<Component>(mixer.inputs); //it is dumb Unity can't inspect Lists without a custom editor
+			temp.Add(osc.GetComponent<OutBridge>());
+			mixer.inputs = temp.ToArray();//WET FLOOR: this code is bad and I feel bad. But the fix is to dupe AddAndLevel using a List<> instead of an array, write a custom inspector (not actually that hard), or pull out "bundle of inputs" and make mixer take that instead of having its own collection, then have one of those with an array and one with a list. Fixes without taking either step, but will be confusing to work with unless we bury all these components behind the scenes. "bridge" is already pretty sketch.
+			return osc;
 		}
 	}
 
 	void NoteOn(MidiChannel channel, int note, float velocity)
 	{
+		if (velocity == 0f)
+		{
+			//NoteOff(channel, note);
+			return;//my keyboard is weird. Is this normal? Note on at 0 and then off?
+		}
 		if (channel != this.channel) {
 			return;
 		}
@@ -53,9 +64,10 @@ public class OscillatorRack : MonoBehaviour
 		bridge.vol = velocity;
 
 		playingOscillators[note] = osc;
-		var temp = new List<Component>(mixer.inputs); //it is dumb Unity can't inspect Lists without a custom editor
-		temp.Add(osc.GetComponent<OutBridge>());
-		mixer.inputs = temp.ToArray();//WET FLOOR: this code is bad and I feel bad. But the fix is to dupe AddAndLevel using a List<> instead of an array, write a custom inspector (not actually that hard), or pull out "bundle of inputs" and make mixer take that instead of having its own collection, then have one of those with an array and one with a list. Fixes without taking either step, but will be confusing to work with unless we bury all these components behind the scenes. "bridge" is already pretty sketch.
+		
+		//var temp = new List<Component>(mixer.inputs); //it is dumb Unity can't inspect Lists without a custom editor
+		//temp.Add(osc.GetComponent<OutBridge>());
+		//mixer.inputs = temp.ToArray();//WET FLOOR: this code is bad and I feel bad. But the fix is to dupe AddAndLevel using a List<> instead of an array, write a custom inspector (not actually that hard), or pull out "bundle of inputs" and make mixer take that instead of having its own collection, then have one of those with an array and one with a list. Fixes without taking either step, but will be confusing to work with unless we bury all these components behind the scenes. "bridge" is already pretty sketch.
 	}
 
 	void NoteOff(MidiChannel channel, int note)
@@ -67,9 +79,11 @@ public class OscillatorRack : MonoBehaviour
 		var osc = playingOscillators[note];
 		playingOscillators.Remove(note);
 		unusedOscillators.Push(osc);
-		var temp = new List<Component>(mixer.inputs);
-		temp.Remove(osc.GetComponent<OutBridge>());
-		mixer.inputs = temp.ToArray();
+		var bridge = osc.GetComponent<InBridge>();
+		bridge.vol = 0f;
+		//var temp = new List<Component>(mixer.inputs);
+		//temp.Remove(osc.GetComponent<OutBridge>());
+		//mixer.inputs = temp.ToArray();
 	}
 
 	void OnEnable()
