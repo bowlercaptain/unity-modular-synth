409e1faa0c975d4d9b79c85537f092fe91c97265
Robert Mock <robertjamesmock@gmail.com>
2017-07-18 00:15:31 -0700

    custom map note control and bugfixes:

---
+++ Assets/CuatomMapNoteControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class CuatomMapNoteControl : NoteControl {
+
+	[System.Serializable]
+	public class noteMap
+	{ public MidiChannel channel; public int inNote; public int outNote; }
+
+	public noteMap[] mappings;
+
+	void NoteOn(MidiChannel channel, int note, float velocity)
+	{
+		print("lp" + note + channel);
+		foreach (noteMap map in mappings) {
+			if (channel == map.channel && map.inNote == note)
+			{
+				StartNote(map.outNote, velocity);
+			} }
+	}
+
+	void NoteOff(MidiChannel channel, int note)
+	{
+		foreach (noteMap map in mappings)
+		{
+			if (channel == map.channel && map.inNote == note)
+			{
+				StopNote(map.outNote);
+			}
+		}
+	}
+
+	void OnEnable()
+	{
+		MidiMaster.noteOnDelegate += NoteOn;
+		MidiMaster.noteOffDelegate += NoteOff;
+	}
+
+	void OnDisable()
+	{
+		MidiMaster.noteOnDelegate -= NoteOn;
+		MidiMaster.noteOffDelegate -= NoteOff;
+	}
+}
